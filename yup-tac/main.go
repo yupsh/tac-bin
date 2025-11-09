a8baa3de49fda9d02300c470cd1a032dcacabeb3
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:51:17 -0800

    tests

--- yup-tac/main.go
+++ yup-tac/main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/urfave/cli/v2"
 
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/tac"
 )
 
@@ -55,7 +55,7 @@ func action(c *cli.Context) error {
 
 	// Add file arguments (or none for stdin)
 	for i := 0; i < c.NArg(); i++ {
-		params = append(params, yup.File(c.Args().Get(i)))
+		params = append(params, gloo.File(c.Args().Get(i)))
 	}
 
 	// Add flags based on CLI options
@@ -71,5 +71,5 @@ func action(c *cli.Context) error {
 
 	// Create and execute the tac command
 	cmd := Tac(params...)
-	return yup.Run(cmd)
+	return gloo.Run(cmd)
 }
